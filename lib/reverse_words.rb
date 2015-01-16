416c0dbb357a24b2491aff68124db8f29338e65d
Kenneth Baldauf <yankeeken@gmail.com>
2015-01-15 20:27:23 -0800

    reverse_words version 0.0.1

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,8 +1,20 @@
 module ReverseWords
-	ReverseWords.run(text)
+	def ReverseWords.run(text)
 		if text.nil? || text.length <=0
 			puts "Must be given a string containing one or more sentences."
 		else
+			sentences = text.split('.')
+			newSentences = ""
+			sentences.each { |sentence|
+				newSentences += ReverseWords.reverse(sentence.strip()) }
+			puts newSentences.strip()
 		end
+	end
+	
+	def ReverseWords.reverse(sentence)
+		words = sentence.split()
+		reversedSentence = ""
+		words.reverse_each { |word| reversedSentence += word + " " }
+		return (reversedSentence.strip() + ". ")
 	end
 end
